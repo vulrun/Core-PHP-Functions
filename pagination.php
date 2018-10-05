1b449b454da904ddcdfd7a042b8c1417d5dba9a2
Web CDN <28586529+webcdn@users.noreply.github.com>
2018-10-05 10:24:32 +0530

    Complete Guide Added

--- pagination.php
+++ pagination.php
@@ -1,38 +1,45 @@
 <?php
 /**
  * @param
- * $total  (int) Total Rows
- * $freq   (int) Rows per page
- * $page   (int) Current Page Number 
+ * $total_rows      (int) Total Rows
+ * $rows_per_page   (int) Rows per page
+ * $page_no         (int) Current Page Number
+ *
+ * $rows_per_page = 50;                                    // Preserve it in DATABASE or in COOKIE or go with static
+ * $page_no = max(1, $_GET['page']);                       // $page_no minimum value should be 1 and fetching from query string
+ * $limit = ($page_no - 1) * $rows_per_page;
+ *
+ * And MySQL Limit would be "LIMIT $limit, $rows_per_page"
+ * Add <?php echo pagination(1090, $rows_per_page, $page_no); ?>
 **/
 
-function pagination($total, $freq, $page){
+function pagination($total_rows = 0, $rows_per_page = 20, $page_no = 1){
 	$li = array();
 	$links = array();
 	$Ist = 1;
-	$Lst = ceil($total / $freq);
+	$Lst = ceil($total_rows / $rows_per_page);
 
 	if( $Lst < 1 ) return;
 
 	// Add current page to the array
-	if( $page > 0 ){
-		$links[] = $page;
+	if( $page_no > 0 ){
+		$links[] = $page_no;
 	}
 
 	// Add the pages around the current page to the array
-	if( $page > 2 ){
-		$links[] = $page - 1;
-		$links[] = $page - 2;
+	if( $page_no > 2 ){
+		$links[] = $page_no - 1;
+		$links[] = $page_no - 2;
 	}
-	if( $page + 2 <= $Lst ){
-		$links[] = $page + 1;
-		$links[] = $page + 2;
+	if( $page_no + 2 <= $Lst ){
+		$links[] = $page_no + 1;
+		$links[] = $page_no + 2;
 	}
-
+	
 
 	// Link to first page, plus ellipses if necessary
 	if( !in_array($Ist, $links) ) {
-		$class = $page == $Ist ? 'active' : '';
+		$class = $page_no == $Ist ? 'active' : '';
 		$li[] = sprintf('<li class="page-item %s"><a class="page-link" href="%s">%s</a></li>', $class, permalink('page', $Ist), $Ist);
 
 		if( !in_array($Ist + 1, $links) ){
@@ -42,7 +49,7 @@ function pagination($total, $freq, $page){
 
 	sort($links);
 	foreach( $links as $link ){
-		$class = $page == $link ? 'active' : '';
+		$class = $page_no == $link ? 'active' : '';
 		$li[] = sprintf('<li class="page-item %s"><a class="page-link" href="%s">%s</a></li>', $class, permalink('page', $link), $link );
 	}
 
@@ -52,7 +59,7 @@ function pagination($total, $freq, $page){
 			$li[] = '<li class="page-item disabled"><a class="page-link" href="#">&hellip;</a></li>';
 		}
 
-		$class = $page == $Lst ? 'active' : '';
+		$class = $page_no == $Lst ? 'active' : '';
 		$li[] = sprintf('<li class="page-item %s"><a class="page-link" href="%s">%s</a></li>', $class, permalink('page', $Lst), $Lst );
 	}
 
