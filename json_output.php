e830ca0c511f6ee2bc273728094ad6e7c9a0a043
Web CDN <28586529+webcdn@users.noreply.github.com>
2019-02-23 18:08:25 +0530

    Update json_output.php

--- json_output.php
+++ json_output.php
@@ -1,5 +1,5 @@
 <?php
-function output($flag, $response, $extra = array()) {
+function json_output($flag, $response, $extra = array()) {
 	header('Content-Type: application/json');
 	$data = array('flag' => (bool) $flag, 'response' => $response);
 	exit(json_encode($data + $extra, JSON_PRETTY_PRINT));
