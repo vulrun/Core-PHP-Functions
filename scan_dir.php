0457bf272d38a4644cf9103ea40e500bb9a5b79c
Anonymous CDN <ironinblood@gmail.com>
2019-03-10 12:55:01 +0530

    Update scan_dir.php

--- scan_dir.php
+++ scan_dir.php
@@ -4,20 +4,20 @@ function scan_dir($path, $opts = null, &$list = array() ){
 	$files = array_diff( $files, array('.', '..') );
 
 	foreach($files as $file){
-		$tmp = stripos($opts, 'fileName') !== false ? $file : "$path/$file";
-		if( is_dir("$path/$file") ){
-			stripos($opts, 'skipDirs' ) === false && array_push($list, $tmp);
+		$fullpath = "$path/$file";
+		if( is_dir($fullpath) ){
+			stripos($opts, 'skipDirs' ) === false && array_push($list, $fullpath);
 			stripos($opts, 'recursive') !== false && scan_dir("$path/$file", $opts, $list);
 		}
 		else {
-			stripos($opts, 'skipFiles') === false && array_push($list, $tmp);
+			stripos($opts, 'skipFiles') === false && array_push($list, $fullpath);
 		}
 	}
 
 	if( stripos($opts, 'dirFirst') !== false ){
 		$dirA = $filA = array();
 		foreach ($list as $l) {
-			is_dir($l) ? array_push($dirA, $l) : array_push($filA, $l);
+			is_dir(realpath($l)) ? array_push($dirA, $l) : array_push($filA, $l);
 		}
 		return array_merge($dirA, $filA);
 	}
