cb0239a2a09e8616000e5b319ed39fa8b540de37
Anonymous CDN <go4webcdn@gmail.com>
2019-04-04 10:03:21 +0530

    Bug Fixed

--- compress_html.php
+++ compress_html.php
@@ -1,10 +1,11 @@
 <?php
 function compress_html($html) {
 	$search = array(
-		'/\>[^\S ]+/s',     // strip whitespaces after tags, except space
-		'/[^\S ]+\</s',     // strip whitespaces before tags, except space
-		'/(\s)+/s',         // shorten multiple whitespace sequences
-		'/<!--(.|\s)*?-->/' // Remove HTML comments
+		'/[\n\t\r]/',        // replace end of line or tab by a space
+		'/\>[^\S ]+/s',      // strip whitespaces after tags, except space
+		'/[^\S ]+\</s',      // strip whitespaces before tags, except space
+		'/(\s)+/s',          // shorten multiple whitespace sequences
+		'/<!--(.*)-->/Uis',  // Remove HTML comments
 	);
 
 	$replace = array(
@@ -12,7 +13,7 @@ function compress_html($html) {
 		'>',
 		'<',
 		'\\1',
-		''
+		'',
 	);
 
 	return preg_replace($search, $replace, $html);
